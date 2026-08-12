7d41491d527580679a361fc78c2e3cd35d9f98b0
SYDNY QUINQUINO <sjquinquino71936@liceo.edu.ph>
2026-08-12 10:20:05 +0800

    Lab Activity 8: MVVM user profile app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    val lifecycleVersion = "2.10.0"
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)

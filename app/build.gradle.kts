f8c54f7e4dfb76bcca9f043e24cfb7f4e089cc7d
SYDNY QUINQUINO <sjquinquino71936@liceo.edu.ph>
2026-08-26 09:45:37 +0800

    Midterm exam: MyRecipeBook dish and recipe CRUD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 }
